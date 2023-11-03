6714898df496c23865ccc56f88316a6630508cd6
Suraj <112235622+isthissuraj@users.noreply.github.com>
2023-11-03 22:09:05 +0530

    api

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,11 +12,28 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
-  final TextStyle _AppBarFont =
-  const TextStyle(fontStyle: FontStyle.italic, fontWeight: FontWeight.bold, fontSize: 32, color: Color(0xffc29843));
+  final TextStyle _AppBarFont = const TextStyle(
+      fontStyle: FontStyle.italic,
+      fontWeight: FontWeight.bold,
+      fontSize: 32,
+      color: Color(0xffc29843));
 
   var searchController = new TextEditingController();
+
+  getRecipe(String query) async {
+    String url =
+        "https://api.edamam.com/api/recipes/v2?type=public&q=$query&app_id=e2d05329&app_key=8ceb35d8b90c0b54b3023e6b058dace2";
+    Response response = await get(Uri.parse(url));
+    Map data = jsonDecode(response.body);
+    log(response.body.toString());
+    print(data);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipe("paratha");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +42,8 @@ class _HomeScreenState extends State<HomeScreen> {
         elevation: 0,
         backgroundColor: Color(0xfffae9c4),
         centerTitle: true,
-        title: Text("Potato",style: _AppBarFont),
+        title: Text("Potato", style: _AppBarFont),
       ),
-
       body: Stack(
         children: [
           Container(
@@ -33,30 +53,25 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           SafeArea(
             child: Column(
-              
               children: [
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 8),
                   margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                   height: 50,
                   width: MediaQuery.of(context).size.width,
-                  
                   decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(24)
-                  ),
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(24)),
                   child: Row(
                     children: [
                       GestureDetector(
-                        onTap: (){
-                          if((searchController.text).replaceAll(" ", "") == ""){
+                        onTap: () {
+                          if ((searchController.text).replaceAll(" ", "") ==
+                              "") {
                             print("blank search");
-                          }else{
-                            Navigator.pushReplacementNamed(context, "/loading",arguments: {
-                              "searchText" : searchController.text,
-                            });
+                          } else {
+                            getRecipe(searchController.text);
                           }
-
                         },
                         child: Container(
                           child: Icon(
@@ -70,23 +85,34 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: TextField(
                           controller: searchController,
                           decoration: const InputDecoration(
-                              border: InputBorder.none, hintText: "You look Hungry!", hintStyle:TextStyle(color: Color(0xfff0c694))),
+                              border: InputBorder.none,
+                              hintText: "You look Hungry!",
+                              hintStyle: TextStyle(color: Color(0xfff0c694))),
                         ),
                       )
                     ],
                   ),
                 ),
                 Container(
-
-                  padding: EdgeInsets.only(left: 20,right: 20),
+                  padding: EdgeInsets.only(left: 20, right: 20),
                   child: Column(
-
                     crossAxisAlignment: CrossAxisAlignment.start,
-
                     children: [
-                      Text("What do you want to cook?", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold,color: Color(0xffc29843)),),
-                      SizedBox(height: 10,),
-                      Text("Let's cook somethig....", style: TextStyle( fontSize: 20, color: Color(0xffc29843)),)
+                      Text(
+                        "What do you want to cook?",
+                        style: TextStyle(
+                            fontSize: 40,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xffc29843)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        "Let's cook somethig....",
+                        style:
+                            TextStyle(fontSize: 20, color: Color(0xffc29843)),
+                      )
                     ],
                   ),
                 )
